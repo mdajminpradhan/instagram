e9d658b48ca4ef65046c1be25230d05ec416b96f
Ajmin Pradhan <md.ajminpradhan@outlook.com>
2020-08-20 17:08:06 +0800

    Updates

---
+++ precache-manifest.565509ddc1dadef2e5d2e8fe5a939b42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "945cd0f96fcb8769b8e8c44162821433",
+    "url": "/instagram/index.html"
+  },
+  {
+    "revision": "90bccfa41ceb886680f7",
+    "url": "/instagram/static/css/main.28326fa1.chunk.css"
+  },
+  {
+    "revision": "6c3ca506217e7688ad72",
+    "url": "/instagram/static/js/2.fcdc8b9a.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/instagram/static/js/2.fcdc8b9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90bccfa41ceb886680f7",
+    "url": "/instagram/static/js/main.3f678470.chunk.js"
+  },
+  {
+    "revision": "060ad16ba7f5db70f5a5",
+    "url": "/instagram/static/js/runtime-main.a87bf05b.js"
+  }
+]);
